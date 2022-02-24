8a010c9f9f8c023eecd271b8022dad16694ef8ec
Colin <colin.diesh@gmail.com>
2022-02-23 17:23:31 -0700

    Initials

--- init.sh
+++ init.sh
@@ -15,16 +15,16 @@ wget -N https://s3.amazonaws.com/jbrowse.org/genomes/volvox/badread.50x.cram.cra
 ## setup jb2web_optim
 jbrowse add-assembly --load copy volvox.fa --out jb2web --force
 jbrowse add-assembly --load copy volvox.fa --out jb2web_optim --force
-for i in volvox-wgsim.{bam,cram} badread.1000x.{bam,cram} badread.50x.{bam,cram} volvox-sorted.{bam,cram}; do
+for i in volvox.sorted.{bam,cram} volvox-wgsim.{bam,cram} badread.1000x.{bam,cram} badread.50x.{bam,cram} volvox-sorted.{bam,cram}; do
   echo $i
-  jbrowse add-track $i --load copy jb2web --trackId $i --force
-  jbrowse add-track $i --load copy jb2web_optim --trackId $i --force
+  jbrowse add-track $i --load copy --out jb2web --trackId $i --force
+  jbrowse add-track $i --load copy --out jb2web_optim --trackId $i --force
 done;
 
 ## copy files to igv and @jbrowse/react-linear-genome-view demos
-for i in volvox-wgsim.{bam,cram} badread.1000x.{bam,cram} badread.50x.{bam,cram} volvox-sorted.{bam,cram}; do
-  echo $i
+for i in volvox.fa volvox.fa.fai volvox-sorted.{bam,cram} volvox-wgsim.{bam,cram} badread.1000x.{bam,cram} badread.50x.{bam,cram} volvox-sorted.{bam,cram}; do
   for j in jb2lgv igvjs; do
+    echo $i $j
     cp $i $j/build
     cp $i.bai $j/build 2>>/dev/null
     cp $i.crai $j/build 2>>/dev/null

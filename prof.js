b2d2e4006575f6bd0cf32d119323e4a07f836304
Colin <colin.diesh@gmail.com>
2022-03-18 09:06:56 -0600

    Updates

--- prof.js
+++ prof.js
@@ -5,7 +5,7 @@ const means = r.results.map(r => r.mean)
 const min = Math.min(...means)
 const final = r.results.map(r => ({
   command: r.command,
-  mean: r.mean.toPrecision(3),
+  mean: r.mean.toPrecision(3) + ' +/- ' + r.stddev.toPrecision(1),
   relative: (r.mean / min).toPrecision(3) + (r.mean === min ? ' 🍏' : ''),
 }))
 

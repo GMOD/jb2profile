a9cdfcaa192b665fca8927f24fbece6a588f3c38
Colin <colin.diesh@gmail.com>
2022-04-11 19:36:36 +0000

    Updates

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+[ ! -d "jb2_165" ] && jbrowse create --tag v1.6.5 jb2_165
+[ ! -d "jb2_167" ] && jbrowse create --tag v1.6.7 jb2_167
+[ ! -d "jb2_169" ] && jbrowse create --tag v1.6.9 jb2_169
+[ ! -d "jb2_main" ] && jbrowse create --branch main jb2_main
+[ ! -d "jb2_noserialize" ] && rm -rf jb2_noserialize && jbrowse create --branch async_feature_details jb2_noserialize && rm -rf jb2_noserialize/config.json
+
+sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm1 python3 python3-pip
+
+pip3 install git+https://github.com/rrwick/Badread.git
+
+
+npm install -g yarn
+npm install -g @jbrowse/img
+npm install -g @jbrowse/cli
+
+yarn
+if [ ! -d "igv.js" ]; then
+        git clone https://github.com/cmdcolin/igv.js
+        cd igv.js
+        yarn
+        yarn build
+        yarn link
+        cd -
+        yarn link igv
+fi
+yarn workspaces run build
+
+## download only chr22 of human data
+# if [ ! -f "ultra-long-ont_hs37d5_phased.cram" ]; then
+#   samtools view 'https://s3.amazonaws.com/jbrowse.org/genomes/hg19/ultra-long-ont_hs37d5_phased.cram' 22 -o ultra-long-ont_hs37d5_phased.cram
+# fi;
+
+
+# wget -N https://jbrowse.org/genomes/hg19/fasta/hg19.fa.gz
+# wget -N https://jbrowse.org/genomes/hg19/fasta/hg19.fa.gz.fai
+# wget -N https://jbrowse.org/genomes/hg19/fasta/hg19.fa.gz.gzi
+
+
+# if [ ! -f "ultra-long-ont_hs37d5_phased.bam" ]; then
+#   echo "Converting ultralong to BAM"
+#   samtools view -@8 -T hg19.fa.gz ultra-long-ont_hs37d5_phased.cram -o ultra-long-ont_hs37d5_phased.bam
+#   samtools index -@8 ultra-long-ont_hs37d5_phased.bam
+# fi;
+
---
+++ load.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+for j in jb2_165 jb2_167 jb2_169 jb2_main jb2_noserialize; do
+  jbrowse add-assembly --load copy volvox.fa --out $j --force
+  for i in 10000 20000 40000 80000 160000 320000 640000; do
+    echo $i $j
+    jbrowse add-track $i.shortread.cram --load symlink --out $j --trackId $i.shortread.cram --force -a volvox
+    jbrowse add-track $i.shortread.bam --load symlink --out $j --trackId $i.shortread.bam --force -a volvox
+  done;
+  for i in 40x 80x 160x 320x 640x 1280x; do
+    echo $i $j
+    jbrowse add-track $i.longread.cram --load symlink --out $j --trackId $i.longread.cram --force -a volvox
+    jbrowse add-track $i.longread.bam --load symlink --out $j --trackId $i.longread.bam --force -a volvox
+  done;
+
+done;
+
+## copy files to igv and @jbrowse/react-linear-genome-view demos
+for i in 10000 20000 40000 80000 160000 320000 640000; do
+  for j in {jb2lgv,igvjs}; do
+    ln -s -f ../../$i.shortread.cram $j/build/
+    ln -s -f ../../$i.shortread.cram.crai $j/build/
+    ln -s -f ../../$i.shortread.bam $j/build/
+    ln -s -f ../../$i.shortread.bam.bai $j/build/
+  done;
+done;
+for i in 40x 80x 160x 320x 640x 1280x; do
+  for j in {jb2lgv,igvjs}; do
+    ln -s -f ../../$i.shortread.cram $j/build/
+    ln -s -f ../../$i.shortread.cram.crai $j/build/
+    ln -s -f ../../$i.shortread.bam $j/build/
+    ln -s -f ../../$i.shortread.bam.bai $j/build/
+  done;
+done;
+
+cp volvox.fa* igvjs/build
+cp volvox.fa* jb2lgv/build
+

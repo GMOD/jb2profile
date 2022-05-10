5bf8268ed2ae5be482129fc1141e4b1a502a5017
Colin <colin.diesh@gmail.com>
2022-05-10 22:45:40 +0000

    Add jb2mod for main

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 sudo apt-get install -yq gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm1 python3 python3-pip unzip minimap2 libcurl4-openssl-dev hyperfine
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
@@ -40,6 +39,8 @@ if [ ! -d "jbrowse" ]; then
 fi
 
 [ ! -d "jb2_175" ] && jbrowse create --tag v1.7.5 jb2_175
+[ ! -d "jb2mod" ] && jbrowse create --branch main jb2mod
+
 
 if [ ! -d "igv.js" ]; then
   git clone https://github.com/cmdcolin/igv.js

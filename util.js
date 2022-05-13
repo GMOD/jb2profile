96de8599ce2038bcdd3efafa2ddabc9226510a89
Colin <colin.diesh@gmail.com>
2022-05-13 07:40:45 +0000

    Add util

--- util.js
+++ util.js
@@ -9,3 +9,13 @@ export function sum(arr, cb = n => n) {
 export function stddev(arr) {
   return Math.sqrt(sum(arr.map(k => (k - mean(arr)) ** 2)) / arr.length)
 }
+
+export function checkFileExistsSync(filepath) {
+  let flag = true
+  try {
+    fs.accessSync(filepath, fs.constants.F_OK)
+  } catch (e) {
+    flag = false
+  }
+  return flag
+}

dc738d11b5fc8485ef051e888827056e49f4bb84
Colin <colin.diesh@gmail.com>
2022-05-18 02:02:21 -0600

    Run styler

--- plot_multi_runtime.R
+++ plot_multi_runtime.R
@@ -2,31 +2,32 @@ library(ggplot2)
 library(patchwork)
 
 
-df = read.csv('runtime_multi_table_processed.csv',sep='\t')
-df$coverage = paste0(df$coverage,'x coverage')
-df$coverage <- factor(df$coverage, levels = c("20x coverage", "50x coverage", "100x coverage","150x coverage"))
-df = df[df$time < 300,]
-bam = df[df$file_type=='bam',]
-bam_sr = bam[bam$read_type=='shortread',]
-bam_lr = bam[bam$read_type=='longread',]
-cram = df[df$file_type=='cram',]
-cram_sr = cram[cram$read_type=='shortread',]
-cram_lr = cram[cram$read_type=='longread',]
+df <- read.csv("runtime_multi_table_processed.csv", sep = "\t")
+df$coverage <- paste0(df$coverage, "x coverage")
+df$coverage <- factor(df$coverage, levels = c("20x coverage", "50x coverage", "100x coverage", "150x coverage"))
+df <- df[df$time < 300, ]
+bam <- df[df$file_type == "bam", ]
+bam_sr <- bam[bam$read_type == "shortread", ]
+bam_lr <- bam[bam$read_type == "longread", ]
+cram <- df[df$file_type == "cram", ]
+cram_sr <- cram[cram$read_type == "shortread", ]
+cram_lr <- cram[cram$read_type == "longread", ]
 
 
-plot<-function(df, title) {
-  ggplot(df, aes(x = num_tracks, y = time)) + 
+plot <- function(df, title) {
+  ggplot(df, aes(x = num_tracks, y = time)) +
     geom_line(aes(color = program)) +
-    geom_point(aes(color = program)) + 
-    geom_errorbar(aes(ymin=time-sd, ymax=time+sd), width=0.1) +
-    labs(y= "time (s)")+
-    facet_grid(~ coverage) +
+    geom_point(aes(color = program)) +
+    geom_errorbar(aes(ymin = time - sd, ymax = time + sd), width = 0.1) +
+    labs(y = "time (s)") +
+    facet_grid(~coverage) +
     ggtitle(title)
 }
 
-ggsave('img/multi.png', 
- (plot(bam_lr, 'BAM longread runtime - rendering multiple tracks')/
-plot(bam_sr, 'BAM shortread runtime - rendering multiple tracks')/
-plot(cram_lr, 'CRAM longread runtime - rendering multiple tracks')/
-plot(cram_sr, 'CRAM shortread runtime - rendering multiple tracks')),width=13,height=11)
-
+ggsave("img/multi.png",
+  (plot(bam_lr, "BAM longread runtime - rendering multiple tracks") /
+    plot(bam_sr, "BAM shortread runtime - rendering multiple tracks") /
+    plot(cram_lr, "CRAM longread runtime - rendering multiple tracks") /
+    plot(cram_sr, "CRAM shortread runtime - rendering multiple tracks")),
+  width = 13, height = 11
+)
--- plot_runtime.R
+++ plot_runtime.R
@@ -2,38 +2,39 @@ library(ggplot2)
 library(patchwork)
 
 
-total = read.csv('runtime_table_processed.csv',sep='\t')
-dfs = split(total, total$window)
-nlist = names(dfs)
+total <- read.csv("runtime_table_processed.csv", sep = "\t")
+dfs <- split(total, total$window)
+nlist <- names(dfs)
 
 
 
 plot <- function(df, title) {
-  ggplot(df, aes(x = coverage, y = time)) + 
+  ggplot(df, aes(x = coverage, y = time)) +
     geom_line(aes(color = program)) +
-    geom_point(aes(color = program)) + 
-    geom_errorbar(aes(ymin=time-sd, ymax=time+sd), width=10) +
-    labs(y= "time (s)")+
+    geom_point(aes(color = program)) +
+    geom_errorbar(aes(ymin = time - sd, ymax = time + sd), width = 10) +
+    labs(y = "time (s)") +
     ggtitle(title)
 }
 
 
-for(i in 1:length(dfs)) {
-  df = dfs[[i]]
-  name = nlist[i]
-  bam = df[df$file_type=='bam',]
-  bam_sr = bam[bam$read_type=='shortread',]
-  bam_lr = bam[bam$read_type=='longread',]
-  cram = df[df$file_type=='cram',]
-  cram_sr = cram[cram$read_type=='shortread',]
-  cram_lr = cram[cram$read_type=='longread',]
+for (i in 1:length(dfs)) {
+  df <- dfs[[i]]
+  name <- nlist[i]
+  bam <- df[df$file_type == "bam", ]
+  bam_sr <- bam[bam$read_type == "shortread", ]
+  bam_lr <- bam[bam$read_type == "longread", ]
+  cram <- df[df$file_type == "cram", ]
+  cram_sr <- cram[cram$read_type == "shortread", ]
+  cram_lr <- cram[cram$read_type == "longread", ]
 
-  ggsave(paste0('img/single_track_',name,'.png'), 
-         (plot(cram_sr, "CRAM shortread")+plot(cram_lr, "CRAM longread")) /
-         (plot(bam_sr, "BAM shortread")+plot(bam_lr, "BAM longread")) +
-          plot_annotation(
-            title = paste0('Time to render single track on ',name,' region'),
-            theme = theme(plot.title = element_text(size = 16))
-          )
-       , width=13)
+  ggsave(paste0("img/single_track_", name, ".png"),
+    (plot(cram_sr, "CRAM shortread") + plot(cram_lr, "CRAM longread")) /
+      (plot(bam_sr, "BAM shortread") + plot(bam_lr, "BAM longread")) +
+      plot_annotation(
+        title = paste0("Time to render single track on ", name, " region"),
+        theme = theme(plot.title = element_text(size = 16))
+      ),
+    width = 13
+  )
 }
